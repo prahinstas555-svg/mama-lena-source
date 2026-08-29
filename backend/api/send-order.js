5fdb21564a0dd10c9c4406dd02a27bf171514f7a
prahinstas555-svg <prahinstas555@gmail.com>
2026-08-29 12:56:03 +0300

    Update send-order.js

--- backend/api/send-order.js
+++ backend/api/send-order.js
@@ -1,6 +1,6 @@
-import nodemailer from 'nodemailer'
+const nodemailer = require('nodemailer')
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
